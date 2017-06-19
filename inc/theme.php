16e9626eba5c1ac5cece68a8d95b3fd762782963
Snownee <1850986885@qq.com>
2017-06-19 23:00:50 +0800

    Fix

--- inc/theme.php
+++ inc/theme.php
@@ -77,11 +77,11 @@ if(!class_exists('Alnico')) {
 
       $arr['v']['mdl-js'] = 1;
       $arr['v']['jq-js'] = 1;
-      $arr['v']['my-js'] = 8;
+      $arr['v']['my-js'] = 9;
       $arr['v']['ll-js'] = 1;
       $arr['v']['cr-js'] = 1;
       $arr['v']['mdl-css'] = 1;
-      $arr['v']['my-css'] = 8;
+      $arr['v']['my-css'] = 9;
       $arr['v']['icons-css'] = 1;
 
       //paths
